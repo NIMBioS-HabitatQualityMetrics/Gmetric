b6a4fab411536fa40a834a3dcf51da20a3bff91c
joannabieri <joanna.a.bieri@gmail.com>
2019-07-21 11:26:00 -0700

    Update GRplots.R
    remove unnecessary plotting package.

--- GRplots.R
+++ GRplots.R
@@ -3,10 +3,7 @@
 # After running and storing the G-metric node and pathway values plots are created of the data.
 # G-metric values are stored in GR_VALUES
 # Gpathway values are stored in GRP_VALUES
-# The plots are created and stored in the working directory as .pdf files. Files with EMB contain embeded fonts for including in .pdf documents.
-
-library(XLConnect)
-library(extrafont)
+# The plots are created and stored in the working directory as .pdf files.
 
 GR_VALUES <- list()
 GRs_VALUES <- list()
@@ -18,9 +15,13 @@ PLOTGR <- matrix(0,nrow=num_nodes,ncol=nrow(delta_range))
 PLOTGAMMA <- matrix(0,nrow=num_nodes,ncol=nrow(delta_range))
 PLOTGRP <- matrix(0,nrow=num_nodes*num_nodes,ncol=nrow(delta_range))
 PLOTGAMMAP <- matrix(0,nrow=num_nodes*num_nodes,ncol=nrow(delta_range))
+
+# For each delta values in the range find the G values and save them for plotting
+print("Calculating G for a range of delta perturbations:")
 for (DEL in delta_range){
   delta <- DEL
   # CALCULATE G for each given delta
+  print(paste( "delta = ", DEL))
   source(paste("../GR.R",sep=""))
   
   # Save G values for each delta
@@ -45,32 +46,32 @@ for(i in 1:num_nodes){
   }
 }
 
-
+print("Preparing fountain plots.")
 ### PLOTS ###
 
 # NODES #
 #
 #
-#
-#
-#
+
 
 pdf(paste(NAME,"_GvsDelta_plots.pdf", sep="")) ## START PLOT - save as .pdf file
 symb <- 0
 plot(c(0),c(0),
      type="l", 
-     main=expression(paste("Contribution, ", G[r], " , as a function of perturbation ", delta)),
-     #sub=paste("Species: ",NAME),
+     main=(paste("Contribution as a function of perturbation ", delta)),
      ylim = c(min(PLOTGR),max(PLOTGR)), 
      xlim = c(min(delta_range),max(delta_range)), 
      xlab=expression(delta),
      ylab="",
      cex.lab=1.75, 
      cex.axis=1.75, 
-     cex.main=1.75)
+     cex.main=1.75
+     )
+axis(side = 1, at = seq(-1,0.5, by=.1), tck = 1, lty = 2, col = "grey", labels = NA)
+axis(side = 2,  tck=1, lty=2, col = "grey", labels = NA)
 corners = par("usr") #Gets the four corners of plot area (x1, x2, y1, y2)
 par(xpd = TRUE) #Draw outside plot area
-text(x = corners[1]-.18, y = mean(corners[3:4]), expression(G[r]), cex=1.75)
+text(x = corners[1]-.19, y = mean(corners[3:4]), expression( italic(G[r]) ), cex=1.75)
 for(i in 1:num_nodes){
   par(new=TRUE)
   yplot <- PLOTGR[i,]
@@ -88,21 +89,16 @@ for(i in 1:num_nodes){
   
 }
 dev.off() ## END PLOT
-embed_fonts(paste(NAME,"_GvsDelta_plots.pdf", sep=""), outfile=paste(NAME,"_GvsDelta_plots_EMB.pdf", sep=""))
 
 # PATHWAYS #
 #
 #
-#
-#
-#
 
-pdf(paste(NAME,"_GRPvsDelta_plots.pdf", sep="")) ## START PLOT - save as .pdf file
+pdf(paste(NAME,"_GRPathvsDelta_plots.pdf", sep="")) ## START PLOT - save as .pdf file
 symb <- 0
 plot(c(0),c(0),
      type="l", 
-     main=expression(paste("Pathway Criticality ", G[r], ", as a function of ", delta)),
-     #sub=paste("Species: ",NAME),
+     main=expression(paste("Pathway Contribution, as a function of ", delta)),
      ylim = c(min(PLOTGRP),max(PLOTGRP)), 
      xlim = c(min(delta_range),max(delta_range)), 
      xlab=expression(delta),
@@ -110,9 +106,11 @@ plot(c(0),c(0),
      cex.lab=1.75, 
      cex.axis=1.75, 
      cex.main=1.75)
+axis(side = 1, at = seq(-1,0.5, by=.1), tck = 1, lty = 2, col = "grey", labels = NA)
+axis(side = 2,  tck=1, lty=2, col = "grey", labels = NA)
 corners = par("usr") #Gets the four corners of plot area (x1, x2, y1, y2)
 par(xpd = TRUE) #Draw outside plot area
-text(x = corners[1]-.12, y = mean(corners[3:4]), expression(G^r), cex=1.75)
+text(x = corners[1]-.19, y = mean(corners[3:4]), expression( italic(G[r]) ), cex=1.75)
 end <- num_nodes*num_nodes
 for(i in 1:end){
   if(PLOTGRP[i,1]>10^(-5)){
@@ -132,5 +130,3 @@ for(i in 1:end){
   }
 }
 dev.off() ## END PLOT
-embed_fonts(paste(NAME,"_GRPvsDelta_plots.pdf", sep=""), outfile=paste(NAME,"_GRPvsDelta_plots_EMB.pdf", sep=""))
-
