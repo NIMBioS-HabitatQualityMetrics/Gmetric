28560843bdbfe7d97a42f9808ed629434ea7405d
joannabieri <joanna.a.bieri@gmail.com>
2019-07-21 11:49:04 -0700

    Add pulliam example code

---
+++ Pulliam_Metapopulation/RunPulliam.R
@@ -0,0 +1,82 @@
+#########################
+# This code sets up and runs the Pulliam hypothetical migration network metrics
+# model data can be found in metric_inputs_adults.xlsx
+# GR.R does the full G-metric calculation and averaging.
+
+
+## USERS MUST SUPPLY INPUT FILES (metric_inputs_name.xlsx) for each class
+## The format of the input files is important
+## One tab (sheet) for each season
+## Node Attributes at the top to include: Population, Survival Rate, Reproduction or Transition rates, and a list of allowed class transitions
+## Then Path Survival rates in matrix form
+## Finally Path Transition rates in matrix form
+
+# Clear Workspace
+rm(list=ls())
+
+#########################
+### USER DEFINED DATA ###
+#########################
+
+seasons <- 1 # Number of seasons or steps in one annual cycle. 
+# This must match number of spreadsheets in input files
+
+num_nodes <- 2 # Number of nodes in the network
+# This must match the number of initial conditions given in input files
+
+NODENAMES <- c("Node 1", "Node 2")
+# Used to name row values in CR outputs, should be ordered to match node order in the .xlsx file
+
+NETNAME <- c("adults") # Give a distinct name for each class as used in input files
+# Input files should be in the same directory as RunSpecies.R and named: metric_inputs_NETNAME[[]].xlsx
+# Order is important when looking at the code: here we would index [[1]] = class 1 and [[2]] = class 2
+
+delta <- -1 #Perturbation amount used in G-metric calculation. delta = -1 means complete node removal
+
+PRINT_RESULTS <- TRUE # If true final CR results will print to the screen
+
+RUN_PERT <- TRUE # If true the code will rerun for each delta value given in delta_range
+# Plots G vs delta and GP vs delta will be created in the directory containing RunMonarchs. -- this could take some time to run
+### RUN_PERT PARAMETERS ###
+delta_range <- matrix(c(.5, .4, .3, .2, .1, 0, -.1, -.2, -.3, -.4, -.5, -.6, -.7, -.8, -.9, -1))
+node_color <- c("orange", "blue") # MEXICO SOUTH CENTRAL NORTH
+NAME <- "Pulliam Metapopulation" # Name for the plots
+
+
+#############################################################
+### Users should not need to interact with the code below ###
+#############################################################
+
+
+# Set the working directory
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+source("../GR.R")
+
+if(PRINT_RESULTS == TRUE){
+  ## Print data to screen
+  print("G-metric for all seasons and nodes:")
+  print(round(GR,4))
+  print("-------------------------------------------------------------------")
+  
+  print("Season averaged G-metric for all nodes:")
+  print(round(GRs,4))
+  print("-------------------------------------------------------------------")
+  
+  print("Network Growth Rate for all seasons:")
+  print(round(LAMBDAt,4))
+  print("-------------------------------------------------------------------")
+  
+  print("G-path-metric for all pathways and seasons:")
+  print(round(GRP,4))
+  print("-------------------------------------------------------------------")
+  
+  print("Season averaged G-path-metric for all pathways:")
+  print(round(GRPs,4))
+  print("-------------------------------------------------------------------")
+  
+}
+
+if(RUN_PERT == TRUE){
+  source(paste("../GRplots.R",sep=""))
+}
